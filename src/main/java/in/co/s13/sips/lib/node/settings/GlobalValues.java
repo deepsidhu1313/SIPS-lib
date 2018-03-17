84a9aefaa30019aced37f2718bba36348b81addc
Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>
2018-03-17 13:51:36 -0500

    * Moved NODE_UUID to node settings package in GlobalValues class. * Updated license headers
    Signed-off-by: Navdeep Singh Sidhu <deepsidhu1313@users.noreply.github.com>

---
+++ src/main/java/in/co/s13/sips/lib/node/settings/GlobalValues.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright (C) 2018 Navdeep Singh Sidhu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package in.co.s13.sips.lib.node.settings;
+
+public class GlobalValues {
+
+    /**
+     * UUID of current Node which is required to identify machine on the SIPS
+     * system.
+     */
+    public static String NODE_UUID = "";
+}
